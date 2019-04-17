2ab874a0075c9d62bc67d7ab905011d16fbf6444
Alexander Mueller <1992alex.m@googlemail.com>
2019-04-17 08:54:55 +0200

    Created Models, Serializer + Loaded Data from json

--- pug-or-ugh_v1/backend/pugorugh/models.py
+++ pug-or-ugh_v1/backend/pugorugh/models.py
@@ -2,3 +2,30 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Dog(models.Model):
+    name = models.CharField(max_length=255)
+    image_filename = models.CharField(max_length=255)
+    breed = models.CharField(max_length=255, default='')
+    age = models.IntegerField()
+    gender = models.CharField(max_length=255, default='u')
+    size = models.CharField(max_length=3, default='u')
+
+    def __str__(self):
+        return self.name
+
+
+class UserDog(models.Model):
+    # This model represents a link between a user and a dog
+
+    user = models.ForeignKey(User, related_name='user_dog_relation')
+    dog = models.ForeignKey(User, related_name='dog_user_relation')
+    status = models.CharField(max_length=1, default='')
+
+
+class UserPref(models.Model):
+    # This model contains the user's preferences
+
+    user = models.ForeignKey(User, related_name='user_pref')
+    age = models.CharField(max_length=255, default='')
+    gender = models.CharField(max_length=255, default='')
+    size = models.CharField(max_length=255, default='')
